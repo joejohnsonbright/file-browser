512a51f6e179a35d9ceadab9b7e2c9e56ee46cf8
Joe Johnson <01johnson.joe+bright@gmail.com>
2025-07-23 23:10:27 +0100

    feat: file component

--- src/components/File.tsx
+++ src/components/File.tsx
@@ -1,16 +1,15 @@
-// Placeholder File component
 interface FileProps {
   name: string;
   type: string;
   added: string;
 }
 
-// eslint-disable-next-line no-unused-vars
 export function File({ name, type, added }: FileProps) {
   return (
-    <div data-testid="file">
-      <span>{name}</span>
-      <p>File placeholder</p>
+    <div data-testid="file" className="flex flex-row space-x-4" role="listitem">
+      <span className="font-medium">{name}</span>
+      <span>{type}</span>
+      <span>{added}</span>
     </div>
   );
 }
